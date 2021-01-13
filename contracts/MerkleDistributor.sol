9bedbf690c28a839e556a3597ca5ce7dd4a17da7
Bram <bramchen77@outlook.com>
2021-01-13 20:41:11 +0300

    feat: reusable merkle distributor

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -7,29 +7,32 @@ import '@openzeppelin/contracts/cryptography/MerkleProof.sol';
 import './interfaces/IMerkleDistributor.sol';
 
 contract MerkleDistributor is Ownable, IMerkleDistributor {
-    address public immutable override token;
-    bytes32 public immutable override merkleRoot;
+    address public override token;
+    bytes32 public override merkleRoot;
+
+    uint256 private mappingVersion;
+    mapping(bytes32 => bool) private claimedMap;
 
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
 
-    constructor(address token_, bytes32 merkleRoot_) public {
+    function setToken(address token_) external onlyOwner {
         token = token_;
+    }
+
+    function setMerkleRoot(bytes32 merkleRoot_) external onlyOwner {
         merkleRoot = merkleRoot_;
+        mappingVersion++;
     }
 
     function isClaimed(uint256 index) public view override returns (bool) {
-        uint256 claimedWordIndex = index / 256;
-        uint256 claimedBitIndex = index % 256;
-        uint256 claimedWord = claimedBitMap[claimedWordIndex];
-        uint256 mask = (1 << claimedBitIndex);
-        return claimedWord & mask == mask;
+        bytes32 key = keccak256(abi.encodePacked(mappingVersion, index));
+        return claimedMap[key];
     }
 
     function _setClaimed(uint256 index) private {
-        uint256 claimedWordIndex = index / 256;
-        uint256 claimedBitIndex = index % 256;
-        claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
+        bytes32 key = keccak256(abi.encodePacked(mappingVersion, index));
+        claimedMap[key] = true;
     }
 
     function claim(
@@ -51,9 +54,9 @@ contract MerkleDistributor is Ownable, IMerkleDistributor {
         emit Claimed(index, account, amount);
     }
 
-    function recoverToken(address receiver) external onlyOwner {
+    function recoverToken(address token_, address receiver_) external onlyOwner {
         require(
-            IERC20(token).transfer(receiver, IERC20(token).balanceOf(address(this))),
+            IERC20(token).transfer(receiver_, IERC20(token_).balanceOf(address(this))),
             'MerkleDistributor: Transfer failed.'
         );
     }
--- migrations/2_deploy_distributor.js
+++ migrations/2_deploy_distributor.js
@@ -1,8 +1,5 @@
 const MerkleDistributor = artifacts.require('MerkleDistributor')
 
-const root = '0x61875005891c53e9b924c6988cf8f612d8bce3472f531f07c5c419abc73d3eaa'
-const ovrToken = '0x8d05f69bd9e804eb467c7e1f2902ecd5e41a72da'
-
 module.exports = function (deployer) {
-  deployer.deploy(MerkleDistributor, ovrToken, root)
+  deployer.deploy(MerkleDistributor)
 }
